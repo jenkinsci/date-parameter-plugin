5d18494967942c785e93bf0bfa15ebe343117b73
JuHyun Lee <leejaycoke@gmail.com>
2017-05-28 21:24:46 +0900

    Add case when build from remote

--- src/main/java/me/leejay/jenkins/dateparameter/DateParameterDefinition.java
+++ src/main/java/me/leejay/jenkins/dateparameter/DateParameterDefinition.java
@@ -1,29 +1,23 @@
 package me.leejay.jenkins.dateparameter;
 
 import hudson.Extension;
-import hudson.cli.CLICommand;
 import hudson.model.ParameterDefinition;
 import hudson.model.ParameterValue;
 import hudson.util.FormValidation;
 import net.sf.json.JSONObject;
-import org.apache.commons.lang.ArrayUtils;
-import org.apache.commons.lang.StringUtils;
 import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.QueryParameter;
 import org.kohsuke.stapler.StaplerRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-
 import static me.leejay.jenkins.dateparameter.utils.LocalDatePattern.*;
+import static org.apache.commons.lang.StringUtils.isEmpty;
 
 /**
  * Created by JuHyunLee on 2017. 5. 23..
  */
 public class DateParameterDefinition extends ParameterDefinition {
-
-    private final static Logger log = LoggerFactory.getLogger(DateParameterDefinition.class);
 
     static final long serialVersionUID = 4;
 
@@ -70,34 +64,26 @@ public class DateParameterDefinition extends ParameterDefinition {
 
     @Override
     public DateParameterValue getDefaultParameterValue() {
-        log.info(">>>>> getDefaultParameterValue called");
-        DateParameterValue v = new DateParameterValue(getName(), "good", getDescription());
-        return v;
+        return new DateParameterValue(getName(), getValue(), getDescription());
     }
 
     @Override
     public ParameterValue createValue(StaplerRequest req, JSONObject jo) {
-        DateParameterValue value = req.bindJSON(DateParameterValue.class, jo);
-        value.setDateFormat(getDateFormat());
-        return value;
+        DateParameterValue dateParameterValue = req.bindJSON(DateParameterValue.class, jo);
+        dateParameterValue.setDateFormat(getDateFormat());
+        return dateParameterValue;
     }
 
     @Override
     public ParameterValue createValue(StaplerRequest staplerRequest) {
-        log.info("createValue2 called");
-        String[] value = staplerRequest.getParameterValues(getName());
-        if (ArrayUtils.isEmpty(value)) {
-            log.info(">>>>> createValue2: empty");
+        String value = staplerRequest.getParameter(getName());
+        if (isEmpty(value)) {
             return getDefaultParameterValue();
         }
 
-        return new DateParameterValue(getName(), value[0], getDescription());
-    }
-
-    @Override
-    public ParameterValue createValue(CLICommand command, String value) throws IOException, InterruptedException {
-        log.info(">>>>> createValue3 called cli");
-        return super.createValue(command, value);
+        DateParameterValue dateParameterValue = new DateParameterValue(getName(), value, getDescription());
+        dateParameterValue.setDateFormat(getDateFormat());
+        return dateParameterValue;
     }
 
     @Extension
@@ -111,14 +97,14 @@ public class DateParameterDefinition extends ParameterDefinition {
         }
 
         public FormValidation doCheckName(@QueryParameter String name) {
-            if (StringUtils.isEmpty(name)) {
+            if (isEmpty(name)) {
                 return FormValidation.error("Please enter a name.");
             }
             return FormValidation.ok();
         }
 
         public FormValidation doCheckDateFormat(@QueryParameter String dateFormat) {
-            if (StringUtils.isEmpty(dateFormat)) {
+            if (isEmpty(dateFormat)) {
                 return FormValidation.error("Please enter a date format");
             }
             return FormValidation.ok();
