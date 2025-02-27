05a4b58c0c180444fc22818c88b44cc26a8f97ff
gounthar <gounthar@gmail.com>
2025-02-27 09:08:44 +0100

    refactor(java): Change variable visibility to private in IntegerParamMethod

--- src/main/java/me/leejay/jenkins/dateparameter/IntegerParamMethod.java
+++ src/main/java/me/leejay/jenkins/dateparameter/IntegerParamMethod.java
@@ -5,19 +5,14 @@ import java.util.regex.Pattern;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-/**
- * Created by JuHyunLee on 2017. 6. 2..
- */
 public class IntegerParamMethod {
 
     private final Logger log = LoggerFactory.getLogger(this.getClass());
 
     private static final Pattern PATTERN = Pattern.compile("^(?<name>.+)\\((?<parameter>[0-9]+)\\);?$");
 
-    public String name;
-
-    public Integer parameter;
-
+    private String name;
+    private Integer parameter;
     private String code;
 
     public IntegerParamMethod(String code) {
